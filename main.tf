6208945e9b4220abc3836e8ec041b680acc8dd92
bradfordwagner <wagner.bradford@gmail.com>
2023-05-16 22:23:30 -0400

    remove script

--- main.tf
+++ main.tf
@@ -22,14 +22,4 @@ resource "kind_cluster" "cluster" {
           protocol: ${port_mapping.protocol}
       %{endfor~}
     EOF
-  # want to take internal kubeconfig
-  # parse server, ca
-  # store in vault
-  # then have it picked up during hydration
-  provisioner "local-exec" {
-    command = "${path.module}/extract_server_ca.sh"
-    environment = {
-      "cluster_name" = var.name
-    }
-  }
 }
