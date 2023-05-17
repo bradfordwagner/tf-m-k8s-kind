15a465bee31ef4dfc48884b962359b34f265c6e0
bradfordwagner <wagner.bradford@gmail.com>
2023-05-16 22:16:40 -0400

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # gh-template-tf-module
+- requires a token to write ca/server to vault
--- extract_server_ca.sh
+++ extract_server_ca.sh
@@ -1,8 +1,15 @@
-kind get kubeconfig --internal --name ${cluster_name} > ~/.kube/kind/internal/${cluster_name}
-export KUBECONFIG=~/.kube/kind/internal/${cluster_name}
+# Create a temporary file
+tmpfile=$(mktemp)
+
+export KUBECONFIG=${tmpfile}
+kind get kubeconfig --internal --name ${cluster_name} > ${KUBECONFIG}
+# export KUBECONFIG=~/.kube/kind/internal/${cluster_name}
 kubectl config view --raw --minify --flatten \
   --output 'jsonpath={.clusters[].cluster.certificate-authority-data}' | base64 --decode \
   > ${KUBECONFIG}_ca
 kubectl config view --raw --minify --flatten \
   --output 'jsonpath={.clusters[].cluster.server}' \
   > ${KUBECONFIG}_server
+
+# cleanup files
+rm ${KUBECONFIG} ${KUBECONFIG}_ca ${KUBECONFIG}_server
